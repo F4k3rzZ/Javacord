8671cb7c9fa02abfbce65926a0569a806770db40
Bastian Oppermann <bastianoppermann1997@gmail.com>
2018-10-27 12:50:21 +0200

    Add dequeue logic

--- javacord-api/src/main/java/org/javacord/api/audio/SilentAudioSource.java
+++ javacord-api/src/main/java/org/javacord/api/audio/SilentAudioSource.java
@@ -1,21 +1,43 @@
 package org.javacord.api.audio;
 
+import java.util.concurrent.TimeUnit;
+
 public class SilentAudioSource implements AudioSource {
 
     public static final byte[] SILENCE_FRAME = {(byte) 0xF8, (byte) 0xFF, (byte) 0xFE};
 
+    private final long initialDuration;
+    private long duration;
+
+    /**
+     * Creates a new silent audio source.
+     *
+     * @param duration How long it should be silent.
+     * @param unit A {@code TimeUnit} determining how to interpret the {@code duration} parameter.
+     */
+    public SilentAudioSource(long duration, TimeUnit unit) {
+        initialDuration = unit.toMillis(duration) / 20;
+        this.duration = initialDuration;
+    }
+
     @Override
     public byte[] getNextFrame() {
-        return SILENCE_FRAME;
+        return null;
     }
 
     @Override
     public boolean hasNextFrame() {
-        return true;
+        duration--;
+        return false;
+    }
+
+    @Override
+    public boolean hasFinished() {
+        return duration <= 0;
     }
 
     @Override
     public AudioSource copy() {
-        return new SilentAudioSource();
+        return new SilentAudioSource(initialDuration * 20, TimeUnit.MILLISECONDS);
     }
 }
